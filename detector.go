891770de638cc9a7117b25da11c73eccd4f4b069
logvinenko_da <logvinenko_da@magnit.ru>
2019-06-11 14:30:29 +0300

    Implements simple web-viewer

--- detector.go
+++ detector.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"html/template"
 	"image"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
@@ -11,9 +14,33 @@ import (
 )
 
 const (
-	URL = "https://cdn.iz.ru/sites/default/files/styles/900x506/public/news-2018-08/20180801_gaf_u39_502.jpg"
+	// HOST is a webserver host
+	HOST = "127.0.0.1"
+	// PORT is a webserver port
+	PORT = 8080
+
+	// PAGE_TPL is an HTML Page template
+	PAGE_TPL = `
+		<!DOCTYPE html>
+		<html>
+			<head></head>
+			<body>
+				<table>
+					{{.}}
+				</table>
+			</body>
+		</html>
+	`
+	// ROW_TPL is an HTML Table row template
+	ROW_TPL = `
+		<tr>
+			<td><img class="image" src="{{.imageSrc}}" width="500" /></td>
+			<td><img class="hist"  src="{{.histSrc}}" /></td>
+		</tr>
+	`
 )
 
+// maxUint32 returns a maximum of uint32 values
 func maxUint32(numbers ...uint32) uint32 {
 	currentMax := numbers[0]
 	for i := 1; i < len(numbers); i++ {
@@ -25,6 +52,7 @@ func maxUint32(numbers ...uint32) uint32 {
 	return currentMax
 }
 
+// makeHistogramm collects average brightness of pixels
 func makeHistogram(m *image.Image) *[256]uint32 {
 	var hist [256]uint32
 
@@ -42,6 +70,7 @@ func makeHistogram(m *image.Image) *[256]uint32 {
 	return &hist
 }
 
+// plotTty prints histogram by values in STDOUT
 func plotTty(values *[256]uint32) {
 	hist := *values
 
@@ -54,6 +83,7 @@ func plotTty(values *[256]uint32) {
 	}
 }
 
+// parseNPlot is an experiments :)
 func parseNPlot(url string) {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -81,6 +111,61 @@ func parseNPlot(url string) {
 	plotTty(&hist)
 }
 
+// server provieds webserver
+func serve(host string, port int, folder string) error {
+	http.HandleFunc("/hist", imagesPageHandler)
+	http.Handle("/", http.FileServer(http.Dir(folder)))
+
+	server := fmt.Sprintf("%s:%d", host, port)
+	log.Printf("Started at https://%s/hist\n", server)
+
+	err := http.ListenAndServe(server, nil)
+
+	return err
+}
+
+// imagesPageHandler builds images pages
+func imagesPageHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s %s", (*r).Method, (*r).URL.String())
+
+	rows := bytes.NewBufferString("")
+
+	for _, image := range *loadImages(".") {
+		if tmpl, err := template.New("Row").Parse(ROW_TPL); err != nil {
+			log.Fatal(err)
+		} else {
+			data := map[string]interface{}{"imageSrc": image}
+			if err = tmpl.Execute(rows, data); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	if tmpl, err := template.New("Page").Parse(PAGE_TPL); err != nil {
+		log.Fatal(err)
+	} else {
+		if err = tmpl.Execute(w, template.HTML(rows.String())); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+// loadImages returns file names in the given folder
+func loadImages(path string) *[]string {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var images []string
+	for _, file := range files {
+		images = append(images, file.Name())
+	}
+
+	return &images
+}
+
 func main() {
-	parseNPlot(URL)
+	// parseNPlot(URL)
+	serve(HOST, PORT, ".")
 }
